2dd72eb8c8f1a87d9c2ebd53f9a2bb6e053e034c
Joe Stephens <joe@webantic.co.uk>
2018-08-14 00:07:45 +0100

    Add font awesome icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import './NavBar.css';
 
 const NavBar = () => (
   <div className="NavBar">
-    <div className="logo">Surreal Estate</div>
+    <div className="logo"><i className="fab fa-fort-awesome" /> Surreal Estate</div>
     <ul className="nav">
       <li className="item">View Properties</li>
       <li className="item">Add a Property</li>
