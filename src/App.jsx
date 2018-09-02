880d030b500462caf3e1b62760d2a80e474160e6
Joe Stephens <joe@mcr.codes>
2018-09-02 18:36:59 +0100

    facebook sso

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Properties from './components/Properties';
 import AddProperty from './components/AddProperty';
 import './App.css';
 
-const App = () => (
-  <Fragment>
-    <NavBar />
-    <Switch>
-      <Route exact path="/" component={Properties} />
-      <Route exact path="/add-property" component={AddProperty} />
-    </Switch>
-  </Fragment>
-);
+class App extends Component {
+  auth2;
+
+  state = {
+    userID: null,
+  };
+
+  handleLogin = ({ userID }) => {
+    console.log(userID);
+    this.setState({ userID });
+  };
+
+  handleLogout = () => this.setState({ userID: null });
+
+  render() {
+    return (
+      <Fragment>
+        <NavBar
+          userID={this.state.userID}
+          onLogin={this.handleLogin}
+          onLogout={this.handleLogout}
+        />
+        <Switch>
+          <Route exact path="/" component={Properties} />
+          <Route exact path="/add-property" component={AddProperty} />
+        </Switch>
+      </Fragment>
+    );
+  }
+}
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import FacebookLogin from 'react-facebook-login';
 import './NavBar.css';
 
-const NavBar = ({ history }) => (
+const NavBar = ({
+  history, userID, onLogin, onLogout,
+}) => (
   <div className="NavBar">
     <div className="logo"><i className="fab fa-fort-awesome" /> Surreal Estate</div>
     <ul className="nav">
       <li className="item" onClick={() => history.push('/')}>View Properties</li>
       <li className="item" onClick={() => history.push('/add-property')}>Add a Property</li>
     </ul>
+    {console.log(userID)}
+    <div className="fblogin">
+      {userID ? <button onClick={() => window.FB.logout(onLogout)}>Sign Out</button> : (
+        <FacebookLogin
+          appId="211809719694152"
+          autoLoad
+          fields="name,email,picture"
+          callback={onLogin}
+        />
+      )}
+    </div>
   </div>
 );
 
